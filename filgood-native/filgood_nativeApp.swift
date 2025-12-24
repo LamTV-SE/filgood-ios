218e594bb930c04d15e3dcacc12ecf6328fd1813
lamtv.dev <lamtv.dev@gmail.com>
2025-12-24 14:08:09 +0700

    Initial Commit

---
+++ filgood-native/filgood_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  filgood_nativeApp.swift
+//  filgood-native
+//
+//  Created by Van Lam on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct filgood_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ filgood-nativeTests/filgood_nativeTests.swift
@@ -0,0 +1,17 @@
+//
+//  filgood_nativeTests.swift
+//  filgood-nativeTests
+//
+//  Created by Van Lam on 24/12/25.
+//
+
+import Testing
+@testable import filgood_native
+
+struct filgood_nativeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
