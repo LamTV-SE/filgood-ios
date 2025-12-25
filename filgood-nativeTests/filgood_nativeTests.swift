bc61629b774800a0eff66d9cce891f3d88f94ce6
lamtv.dev <lamtv.dev@gmail.com>
2025-12-25 10:40:34 +0700

    [CONFIG]: update command format/lint to root

--- filgood-nativeTests/filgood_nativeTests.swift
+++ filgood-nativeTests/filgood_nativeTests.swift
@@ -5,13 +5,11 @@
 //  Created by Van Lam on 24/12/25.
 //
 
-import Testing
 @testable import filgood_native
+import Testing
 
 struct filgood_nativeTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
